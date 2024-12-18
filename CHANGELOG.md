b64a0aa487e8d12bfd51ed1b5cd15d3c16ea96d4
Canary-Prism <canaryprsn@gmail.com>
2024-12-18 18:20:14 +0800

    version 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## v1.0.2
+- fixed `discord-bridge` packaging not being `pom` meaning it couldn't be used as an aggregator
+
 ## v1.0.1
 - fixed `discord-bridge-jda` not having META-INF services defined for non-modular projects
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ allprojects {
     plugins.apply("com.vanniktech.maven.publish")
 
     group = "io.github.canary-prism"
-    version = "1.0.1"
+    version = "1.0.2"
 
     mavenPublishing {
         publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
